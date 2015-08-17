66261b532318fd626b40ff1d4b318c39e37965e9
K. J. Nitthilan <nitthilan@gmail.com>
2015-08-17 19:22:43 +0530

    Added conditional support for running mlp, convnet with and without cuda, with and without full feature set

--- source_code/learning.lua
+++ source_code/learning.lua
@@ -1,9 +1,28 @@
 require 'torch'
-require 'cunn'
-torch.setdefaulttensortype('torch.FloatTensor')
 require 'nn'
 require 'optim'
 require 'constants.lua'
+
+
+-- parse command line arguments
+if not opt then
+   print '==> processing options'
+   cmd = torch.CmdLine()
+   cmd:text()
+   cmd:text('SVHN Model Definition')
+   cmd:text()
+   cmd:text('Options:')
+   cmd:option('-model', 'mlp', 'type of model to construct: mlp | convnet')
+   cmd:option('-type', 'double', 'type: double | cuda')
+   cmd:option('-numdata', 'all_features', 'numdata: all_images | all_features')
+   cmd:text()
+   opt = cmd:parse(arg or {})
+end
+
+if(opt.type == 'cuda') then
+   require 'cunn'
+   torch.setdefaulttensortype('torch.FloatTensor')
+end
 
 torch.setnumthreads(8)--opt.threads)
 torch.manualSeed(1)--opt.seed)
@@ -11,7 +30,7 @@ torch.manualSeed(1)--opt.seed)
 -- Loading the pre-processed data 
 local feature_data = torch.load(FILEPATH_DATA_DIR.."feature_data.raw", 'binary')
 local image_data = torch.load(FILEPATH_DATA_DIR.."image_data.raw", 'binary')
-image_data = image_data:float()
+-- image_data = image_data:float()
 
 local csvFile = io.open(FILEPATH_DATA_DIR..'training.csv', 'r')  
 local header = csvFile:read()
@@ -28,18 +47,20 @@ end
 -- finding out all images which have all the 30 features
 local num_images = 0
 local image_id_map = {}
-for i=1,MAX_TRAIN_IMG do
-   --[[
-	local byte_vec = torch.ne(feature_data:select(1,i), -1.0)
-	if torch.sum(byte_vec) == MAX_FEATURE then
-		num_images = num_images + 1
-		image_id_map[num_images] = i
-	end
-   --]]
-   
-   num_images = num_images + 1
-   image_id_map[num_images] = i
-   
+
+if opt.numdata == 'all_features' then
+   for i=1,MAX_TRAIN_IMG do
+   	local byte_vec = torch.ne(feature_data:select(1,i), -1.0)
+   	if torch.sum(byte_vec) == MAX_FEATURE then
+   		num_images = num_images + 1
+   		image_id_map[num_images] = i
+   	end
+   end
+else
+   for i=1,MAX_TRAIN_IMG do
+      num_images = num_images + 1
+      image_id_map[num_images] = i
+   end
 end
 print ("Num images with all the 30 feature vectors ", num_images)
 -- Calculating the 80% of total images as the training data set and the rest as the test data set to validate the training
@@ -71,8 +92,10 @@ local criterion = nn.MSECriterion()
 -- criterion.sizeAverage = false
 
 
-model:cuda()
-criterion:cuda()
+if opt.type == 'cuda' then
+   model:cuda()
+   criterion:cuda()
+end
 -- model:float()
 
 
@@ -110,9 +133,16 @@ feval = function(x_new)
    if _nidx_ > num_images_training then _nidx_ = 1 end
 
    local image_id = image_id_map[_nidx_]
-   -- local inputs = image_data[image_id]
-   local inputs = image_data[image_id]:view(1, 96,96)
-   inputs = inputs:cuda()
+   local inputs
+   if opt.model == 'mlp' then
+      inputs = image_data[image_id]
+   else
+      inputs = image_data[image_id]:view(1, 96,96)
+   end
+   
+   if opt.type == 'cuda' then
+      inputs = inputs:cuda()
+   end
    -- print (inputs:dim(), inputs:size())
    local target = feature_data[image_id]
 
@@ -124,19 +154,30 @@ feval = function(x_new)
 
    -- Logic to make the predicted output to target output so that it does not affect the error calculation
    -- 
-   local byte_vec_fea = torch.ne(target, -1.0)
-   local byte_vec_non_fea = torch.eq(target, -1.0)
-   local forward_output = model:forward(inputs)   
-   -- print (forward_output)
+   local loss_x
+   if opt.numdata == 'all_images' then
+      local forward_output = model:forward(inputs)   
+      local byte_vec_fea = torch.ne(target, -1.0)
+      local byte_vec_non_fea = torch.eq(target, -1.0)
+      local zeroed_target, selected_output, equalised_target
+      if opt.type == 'cuda' then
+         zeroed_target = torch.cmul(target:cuda(), byte_vec_fea:cuda())
+         selected_output = torch.cmul(forward_output:cuda(), byte_vec_non_fea:cuda())
+         equalised_target = torch.add(zeroed_target:cuda(), selected_output:cuda())
+      else
+         zeroed_target = torch.cmul(target:double(), byte_vec_fea:double())
+         selected_output = torch.cmul(forward_output:double(), byte_vec_non_fea:double())
+         equalised_target = torch.add(zeroed_target:double(), selected_output:double())
+      end
+      -- print(byte_vec_fea, byte_vec_non_fea, forward_output, zeroed_target, selected_output, equalised_target)
+      -- evaluate the loss function and its derivative wrt x, for that sample
 
-   local zeroed_target = torch.cmul(target, byte_vec_fea:double())
-   local selected_output = torch.cmul(forward_output:cuda(), byte_vec_non_fea:cuda())
-   local equalised_target = torch.add(zeroed_target:cuda(),selected_output:cuda())
-   -- print(byte_vec_fea, byte_vec_non_fea, forward_output, zeroed_target, selected_output, equalised_target)
-   -- evaluate the loss function and its derivative wrt x, for that sample
-
-   local loss_x = criterion:forward(forward_output, equalised_target)
-   model:backward(inputs, criterion:backward(model.output, equalised_target))
+      loss_x = criterion:forward(forward_output, equalised_target)
+      model:backward(inputs, criterion:backward(model.output, equalised_target))
+   else
+      loss_x = criterion:forward(model:forward(inputs), target)
+      model:backward(inputs, criterion:backward(model.output, target))
+   end
 
    -- return loss(x) and dloss/dx
    return loss_x, dl_dx
@@ -170,6 +211,9 @@ for epoch = 1,1e4 do
    -- this variable is used to estimate the average loss
    current_loss = 0
 
+   -- local vars
+   local time = sys.clock()
+
    -- an epoch is a full loop over our training data
    for img_id = 1,num_images_training do
 
@@ -193,24 +237,43 @@ for epoch = 1,1e4 do
       -- print ("image error ", fs[1])
    end
 
+   -- time taken
+   time = sys.clock() - time
+   time = time / num_images_training
+   print("\n==> time to learn 1 sample = " .. (time*1000) .. 'ms')
+
    -- report average error on epoch
    current_loss = current_loss / num_images_training
    print(epoch..' current loss = ' .. current_loss)
    -- Validating the trained model using validating data set
    local validation_loss = 0.0
+   time = sys.clock()
    for i = num_images_training,num_images do
       local image_id = image_id_map[i]
-      -- local inputs = image_data[image_id]
-      local inputs = image_data[image_id]:view(1, 96,96)
-      inputs = inputs:cuda()
+      if opt.model == 'mlp' then
+         inputs = image_data[image_id]
+      else
+         inputs = image_data[image_id]:view(1, 96,96)
+      end
+      
+      if opt.type == 'cuda' then
+         inputs = inputs:cuda()
+      end
       local target = feature_data[image_id]
       local forward_output = model:forward(inputs)
       
       local byte_vec_fea = torch.ne(feature_data:select(1,image_id), -1.0)
       local byte_vec_non_fea = torch.eq(feature_data:select(1,image_id), -1.0)
-      local zeroed_target = torch.cmul(target, byte_vec_fea:double())
-      local selected_output = torch.cmul(forward_output, byte_vec_non_fea:cuda())
-      local equalised_target = torch.add(zeroed_target:cuda(),selected_output:cuda())
+      local zeroed_target, selected_output, equalised_target
+      if opt.type == 'cuda' then
+         zeroed_target = torch.cmul(target:cuda(), byte_vec_fea:cuda())
+         selected_output = torch.cmul(forward_output:cuda(), byte_vec_non_fea:cuda())
+         equalised_target = torch.add(zeroed_target:cuda(), selected_output:cuda())
+      else
+         zeroed_target = torch.cmul(target:double(), byte_vec_fea:double())
+         selected_output = torch.cmul(forward_output:double(), byte_vec_non_fea:double())
+         equalised_target = torch.add(zeroed_target:double(), selected_output:double())
+      end
       local error = equalised_target - forward_output
       -- local mse1 = math.sqrt(torch.mean(torch.pow(error, 2)))
       local mse = torch.norm(error)/math.sqrt(torch.sum(byte_vec_fea));
@@ -218,6 +281,10 @@ for epoch = 1,1e4 do
       validation_loss = validation_loss + mse
    end
    print(epoch..' current validation loss '.. validation_loss/num_images_validating)
+   -- time taken
+   time = sys.clock() - time
+   time = time / num_images_training
+   print("\n==> time to validate 1 sample = " .. (time*1000) .. 'ms')
 
    -- saving the model for using it later
    modsav = model:clone('weight', 'bias');
--- source_code/model.lua
+++ source_code/model.lua
@@ -8,74 +8,74 @@ require 'constants.lua'
 -- Try one: 1-D without convolution
 model = nn.Sequential() 
 
---[[
-ninputs = IMG_DIM*IMG_DIM; nhidden = 100; noutputs = MAX_FEATURE
-model:add(nn.Linear(ninputs, nhidden))
-model:add(nn.ReLU())
-model:add(nn.Linear(nhidden, noutputs)) 
---]]
+if opt.model == 'mlp' then
+	ninputs = IMG_DIM*IMG_DIM; nhidden = 100; noutputs = MAX_FEATURE
+	model:add(nn.Linear(ninputs, nhidden))
+	model:add(nn.ReLU())
+	model:add(nn.Linear(nhidden, noutputs)) 
+elseif opt.model == 'convnet' then
+	if opt.type == 'double' then
+		-- hidden units, filter sizes (for ConvNet only):
+		nfeats=1
+		nstates = {64,64,128}
+		filtsize = 5
+		poolsize = 2
+		normkernel = image.gaussian1D(7)
+		noutputs = MAX_FEATURE
+		-- a typical convolutional network, with locally-normalized hidden
+		-- units, and L2-pooling
+
+		-- Note: the architecture of this convnet is loosely based on Pierre Sermanet's
+		-- work on this dataset (`). In particular
+		-- the use of LP-pooling (with P=2) has a very positive impact on
+		-- generalization. Normalization is not done exactly as proposed in
+		-- the paper, and low-level (first layer) features are not fed to
+		-- the classifier.
+		-- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
+		model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
+		model:add(nn.Tanh())
+		model:add(nn.SpatialLPPooling(nstates[1],2,poolsize,poolsize,poolsize,poolsize))
+		model:add(nn.SpatialSubtractiveNormalization(nstates[1], normkernel))
+
+		-- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
+		model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
+		model:add(nn.Tanh())
+		model:add(nn.SpatialLPPooling(nstates[2],2,poolsize,poolsize,poolsize,poolsize))
+		model:add(nn.SpatialSubtractiveNormalization(nstates[2], normkernel))
+
+		-- stage 3 : standard 2-layer neural network
+		model:add(nn.Reshape(21*21*nstates[2])) -- nstates[2]*filtsize*filtsize))
+		model:add(nn.Linear(21*21*nstates[2], nstates[3])) 
+		model:add(nn.Tanh())
+		model:add(nn.Linear(nstates[3], noutputs))
+	elseif opt.type == 'cuda' then
 
 
---[[
--- hidden units, filter sizes (for ConvNet only):
-nfeats=1
-nstates = {64,64,128}
-filtsize = 5
-poolsize = 2
-normkernel = image.gaussian1D(7)
-noutputs = MAX_FEATURE
--- a typical convolutional network, with locally-normalized hidden
--- units, and L2-pooling
+		-- hidden units, filter sizes (for ConvNet only):
+		nfeats=1
+		nstates = {64,64,128}
+		filtsize = 5
+		poolsize = 2
+		normkernel = image.gaussian1D(7)
+		noutputs = MAX_FEATURE
+		-- a typical modern convolution network (conv+relu+pool)
+		model = nn.Sequential()
 
--- Note: the architecture of this convnet is loosely based on Pierre Sermanet's
--- work on this dataset (`). In particular
--- the use of LP-pooling (with P=2) has a very positive impact on
--- generalization. Normalization is not done exactly as proposed in
--- the paper, and low-level (first layer) features are not fed to
--- the classifier.
--- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
-model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
-model:add(nn.Tanh())
-model:add(nn.SpatialLPPooling(nstates[1],2,poolsize,poolsize,poolsize,poolsize))
-model:add(nn.SpatialSubtractiveNormalization(nstates[1], normkernel))
+		-- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
+		model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
+		model:add(nn.ReLU())
+		model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
 
--- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
-model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
-model:add(nn.Tanh())
-model:add(nn.SpatialLPPooling(nstates[2],2,poolsize,poolsize,poolsize,poolsize))
-model:add(nn.SpatialSubtractiveNormalization(nstates[2], normkernel))
+		-- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
+		model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
+		model:add(nn.ReLU())
+		model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
 
--- stage 3 : standard 2-layer neural network
-model:add(nn.Reshape(21*21*nstates[2])) -- nstates[2]*filtsize*filtsize))
-model:add(nn.Linear(21*21*nstates[2], nstates[3])) 
-model:add(nn.Tanh())
-model:add(nn.Linear(nstates[3], noutputs))
---]]
-
-
--- hidden units, filter sizes (for ConvNet only):
-nfeats=1
-nstates = {64,64,128}
-filtsize = 5
-poolsize = 2
-normkernel = image.gaussian1D(7)
-noutputs = MAX_FEATURE
--- a typical modern convolution network (conv+relu+pool)
-model = nn.Sequential()
-
--- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
-model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
-
--- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
-model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
-model:add(nn.ReLU())
-model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
-
--- stage 3 : standard 2-layer neural network
-model:add(nn.View(nstates[2]*21*21))
-model:add(nn.Dropout(0.5))
-model:add(nn.Linear(nstates[2]*21*21, nstates[3]))
-model:add(nn.ReLU())
-model:add(nn.Linear(nstates[3], noutputs))
+		-- stage 3 : standard 2-layer neural network
+		model:add(nn.View(nstates[2]*21*21))
+		model:add(nn.Dropout(0.5))
+		model:add(nn.Linear(nstates[2]*21*21, nstates[3]))
+		model:add(nn.ReLU())
+		model:add(nn.Linear(nstates[3], noutputs))
+	end
+end
